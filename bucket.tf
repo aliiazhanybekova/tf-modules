bf398d01bfe84b5d20e8fd08621c2663b00bb61e
aliiazhanybekova <alex.zhanybekova@gmail.com>
2024-04-21 00:40:07 -0400

    added

--- bucket.tf
+++ bucket.tf
@@ -2,3 +2,9 @@ resource "google_storage_bucket" "gcs_bucket" {
   name          = var.bucket_name
   location      = var.location
 }
+
+resource "google_storage_bucket_iam_member" "terraform_state_bucket_viewer" {
+  bucket = google_storage_bucket.gcs_bucket.name
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:devopshub21@gmail.com"
+}
